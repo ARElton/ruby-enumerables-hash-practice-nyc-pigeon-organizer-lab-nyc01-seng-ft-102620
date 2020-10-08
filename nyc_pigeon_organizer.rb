edb47263a4bff0ab31faa3ad1c2aaf01a815c926
Amelia Elton <ameliareneeelton@gmail.com>
2020-10-08 19:03:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,11 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
       names.each do |name|
-        binding.pry 
+        if | final_array[name]
+          final_array[name] = {}
+          binding.pry
+        end
+      end
     end
   end
     final_array
